186e584d08273c30f17d7f9631f58a3d55aa4089
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-06 17:57:00 -0500

    Moving

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import MovingObject from "./moving_object.js";
 
 const mo = new MovingObject({
     pos: [100, 100],
-    vel: [10, 10],
+    vel: [20, 20],
     radius: 100,
-    color: "#00FF00"
+    color: "#808080"
   });
 
 window.MovingObject = MovingObject;
@@ -14,6 +14,8 @@ window.MovingObject = MovingObject;
 document.addEventListener("DOMContentLoaded", function(){
     window.canvas = document.getElementById("game-canvas");
     window.ctx = canvas.getContext("2d");
+    window.canvas.height = window.innerHeight;
+    window.canvas.width = window.innerWidth;
 });
 
 window.mo = mo;
--- src/moving_object.js
+++ src/moving_object.js
@@ -1,3 +1,5 @@
+import { ContextExclusionPlugin } from "webpack";
+
 class MovingObject {
     constructor(obj) {
         this.pos = obj.pos;
@@ -21,15 +23,16 @@ class MovingObject {
 
         ctx.fill();
     }
+
+    move() {
+        let newX = this.pos[0] + this.vel[0];
+        let newY = this.pos[1] + this.vel[1];
+        this.pos = [newX, newY];
+        // ctx.clearRect(0,0, window.canvas.width, window.canvas.height);
+        // this.draw(ctx);
+    }
 }
 
-// class MovingObject {
-//     constructor({options}) {
-//         // this.pos = options[pos];
-//         // this.vel = options[vel];
-//         // this.radius = options[radius];
-//         // this.color = options[color];
-//     }
-// }
+
 
 export default MovingObject;
