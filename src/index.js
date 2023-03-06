fe305759c45ad2932dddf40076783c092d629d4b
“Kelsang” <kelsangrtsering@gmail.com>
2023-03-06 17:34:32 -0500

    work on circle instance

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ console.log("Webpack is working!")
 
 import MovingObject from "./moving_object.js";
 
+const mo = new MovingObject({
+    pos: [100, 100],
+    vel: [10, 10],
+    radius: 100,
+    color: "#00FF00"
+  });
+
 window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", function(){
+    window.canvas = document.getElementById("game-canvas");
+    window.ctx = canvas.getContext("2d");
+});
+
+window.mo = mo;
--- src/moving_object.js
+++ src/moving_object.js
@@ -1,9 +1,25 @@
 class MovingObject {
-    constructor(pos, vel, radius, color) {
-        this.pos = pos;
-        this.vel = vel;
-        this.radius = radius;
-        this.color = color;
+    constructor(obj) {
+        this.pos = obj.pos;
+        this.vel = obj.vel;
+        this.radius = obj.radius;
+        this.color = obj.color;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+
+        ctx.arc(
+            this.pos[0],
+            this.pos[1],
+            this.radius,
+            0,
+            2*Math.PI,
+            false
+        );
+
+        ctx.fill();
     }
 }
 
